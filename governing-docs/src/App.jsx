7cdbc5ae04e9944a924279bcb9f0fc7a106d52d4
fahim484 <fahim.faysal.webdeveloper@gmail.com>
2024-10-15 10:26:24 +0600

    hero section done

--- governing-docs/src/App.jsx
+++ governing-docs/src/App.jsx
@@ -1,14 +1,15 @@
 import { Button } from "./components/common/Button";
 import { SectionTitle } from "./components/common/SectionTitle";
+import { Hero } from "./components/home/Hero";
 import { Footer } from "./components/shared/Footer";
 import { MobileMenu } from "./components/shared/MobileMenu";
 import { Navbar } from "./components/shared/Navbar";
 
 const App = () => {
   return (
-    <main className="">
+    <main className="font-nunito-sans bg-[url(Img/hero-bg.svg)] min-h-svh bg-no-repeat bg-top">
       <Navbar />
-      <div className="min-h-[50svh]"></div>
+      <Hero />
       <Footer />
     </main>
   );
